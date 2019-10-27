700ec80cb26329fdea5fde142c30c9dab9177e97
James Whale <james@james-whale.com>
2019-10-27 01:49:35 +0100

    Modified code to work on Chromium.

--- chrome/background.js
+++ chrome/background.js
@@ -1,14 +1,11 @@
 chrome.runtime.onInstalled.addListener(function listenertst() {
-	function listenertst(){
-		console.log("listenertst");
-	});		
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function Clricon() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'mail.google.com'},
-			})
-		],
+	console.log("listenertst");
+});
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function clricon() {
+	chrome.declarativeContent.onPageChanged.addRules([{
+		conditions: [new chrome.declarativeContent.PageStateMatcher({
+			pageUrl: {hostEquals: 'mail.google.com'},
+		})],
 		actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
+	}]);
 });
