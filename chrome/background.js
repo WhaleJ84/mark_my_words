80a9a98fad045020303b8f30a82ce1b44ed0f589
James Whale <james@james-whale.com>
2019-10-27 04:50:38 +0000

    Continued work.

--- chrome/background.js
+++ chrome/background.js
@@ -1,13 +1,12 @@
 chrome.runtime.onInstalled.addListener(function listenerTest() {
-		console.log("listenerTest");
+	console.log("listenerTest");
 });		
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function clearIcon() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'extensions'},
-			})
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function clearIcon() {
+	chrome.declarativeContent.onPageChanged.addRules([{
+		conditions: [new chrome.declarativeContent.PageStateMatcher()
+			//pageUrl: {hostEquals: ''},hrome.runtime.onInstalled.addListener(function listenerTest() {
 		],
 		actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
+	}]);
+});
 
